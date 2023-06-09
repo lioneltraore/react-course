10acd401ea5728d9f0cd0a003f5845cdd1281bdd
Lionel Traore <hello@lioneltraore.com>
2023-06-09 14:12:08 +0000

    add expense component

---
+++ project-init/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 27th 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$300.43</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
