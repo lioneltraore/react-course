93e75cdff7659b60bac38c02d9ddb32330e0e35a
Lionel Traore <hello@lioneltraore.com>
2023-06-19 07:33:23 +0000

    add chart

---
+++ project-init/src/components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import ChartBar from "./ChartBar";
+
+function Chart(props) {
+
+    return (
+        <div className="chart">
+            {
+                props.dataPoints.map(
+                    dataPoint => <ChartBar 
+                    key={dataPoint.label}
+                    value={dataPoint.value} 
+                    maxValue={null} 
+                    label={dataPoint.label} 
+                    />
+                )
+            }
+        </div>
+    )
+
+}
+
+export default Chart;
---
+++ project-init/src/components/Chart/ChartBar.js
@@ -0,0 +1,5 @@
+function ChartBar() {
+
+}
+
+export default ChartBar;
